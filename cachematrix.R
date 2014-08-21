afc48573d9f55444fcf337ab0f8924321816b106
jones00 <jmenezes@gmail.com>
2014-08-20 23:50:23 -0300

    added checks for the class of the new object

--- cachematrix.R
+++ cachematrix.R
@@ -3,20 +3,26 @@
 # same calculations multiple times.
 
 
-## makeCacheMatrix has parameter x, any object of the class matrix and sets up a free variable to cache the inverse.
-# it returns matrix as the new object.
+## makeCacheMatrix has parameter x, an object of the class matrix and sets up a free variable to cache the inverse.
+# it returns the x matrix as the new cached.matrix object.
 makeCacheMatrix <- function(x = matrix()) {
     globalCachedMatrix <<- NULL
+    class(x) <- "cached.matrix"
     x
 }
 
 
-## cacheSolve calculates the inverse of a matrix x and sets the freevariable.
+## cacheSolve calculates the inverse of a matrix x and sets the free variable.
 # It always returns the inverse of x regardless of it been previously cached or not
 cacheSolve <- function(x, ...) {
+  if(class(x) == "cached.matrix"){
   if(is.null(globalCachedMatrix)){
+    #The inverse is not cached, solve it and then cache it.
         globalCachedMatrix <<- solve(x, ...)
   }
-  #there is (now, at least) a cached value, return it
+  #there is a cached value, return it.
   globalCachedMatrix
+  } else {
+    print("Error: Object is not inicialized with makeCachedMatrix()." )
+  }
 }
